42f342512d9a03a61e917ec7481f162957dbcad4
Tobia <tobias@taekker.net>
2022-11-25 13:01:50 +0100

    Poster

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 
-const Title = ({name,handleClick, allinfo}) =>{
+const Title = ({name,handleClick,all}) =>{
     return (
         <div className="container p-5 my-5 bg-dark text-white" key ={name}>
-               <h1>{name}</h1>
-            
+               <h1>{all.titleName}</h1>
+               <img className="poster" src={all.poster} alt={""}/>
                <div className="buttondelete">
-                      <Button variant ="primary" onClick={ () =>handleClick(name)} className>
+                      <Button variant ="primary" onClick={ () =>handleClick(all.titleName)} className>
                         delet this
                      </Button>
               </div>
--- src/TitleList.jsx
+++ src/TitleList.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Title from "./Title";
 
-const TitleList =  ({name, handleClick}) =>{
+const TitleList =  ({name, handleClick,all}) =>{
+
+
+
 return (
  <>
-{name.map(name => <Title name={name} handleClick={handleClick} key={name}/>)}
+ {all.map(all => <Title all={all} key={all.titleId} handleClick={handleClick}/>)}
+ {/*
+{name.map(name => <Title name={name} handleClick={handleClick} key={name}  />)}
+*/}
+
+
+
 </>
 )
 }
