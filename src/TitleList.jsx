33bed0aa98a9055f4efd4b7bd7b0dfc8add78c54
mariabroemarstrand <mariabroemarstrand@gmail.com>
2022-11-28 09:03:52 +0100

    .

--- src/TitleList.jsx
+++ src/TitleList.jsx
@@ -8,12 +8,6 @@ const TitleList =  ({name, handleClick,all}) =>{
 return (
  <>
  {all.map(all => <Title all={all} key={all.titleId} handleClick={handleClick}/>)}
- {/*
-{name.map(name => <Title name={name} handleClick={handleClick} key={name}  />)}
-*/}
-
-
-
 </>
 )
 }
