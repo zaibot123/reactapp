7f6e51950706da30524be2062b888d0a32e0c66e
Tobia <tobias@taekker.net>
2022-11-29 15:07:02 +0100

    gg

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import dataJson from './data.json'
 function App() {
   let [items,setTitleList] =useState(dataJson['items']);
 
+  useEffect(() => {
+    fetch('http://localhost:5001/api/movies/tt3913192/similar')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    }, []);
+
   function handleDelete(titleInput){  
     const titlesList= items.filter(title=>title.titleName!==titleInput);
     setTitleList(titlesList);
--- src/Title.jsx
+++ src/Title.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 
 const Title = ({name,handleClick,all}) =>{
+    console.log(all)
     return (
-        <div className="container p-5 my-5 bg-dark text-white" key ={name}>
+        <div className="container p-5 my-2 bg-dark text-white" key ={name}>
+            <div className="title">
                <h1>{all.titleName}</h1>
+               <p>{all.titleType}</p>
+
+               </div>
                <img className="poster" src={all.poster} alt={""}/>
+               <ul> 
+                <li>number of ratings: {all.nrRatings}</li>
+                <li>average rating: {all.avgRating}</li>
+                <li>runtime: {all.runtime}</li>
+               </ul>
                <div className="buttondelete">
                       <Button variant ="primary" onClick={ () =>handleClick(all.titleName)} className>
                         delet this
