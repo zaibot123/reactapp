b7178512dce6bc57c09c3899641bd56cab546316
mariabroemarstrand <mariabroemarstrand@gmail.com>
2022-12-07 16:07:59 +0100

    photo og navn virker. bookmarks virker ikke endnu, men det er den der arbejdes på

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -4,13 +4,14 @@ import { useState, useEffect} from 'react';
 import {useParams} from "react-router-dom";
 
 
-export default function EditUser() {
+export default function UserProfile() {
 
 
   let [username, setUsername]=useState(null)
   let [email, setEmail]=useState(null)
   let [photo, setPhoto]=useState(null)
   let [bio, setBio]=useState(null)
+  let [poster, setPoster]=useState(null)
 
 
   const {userName} = useParams()
@@ -19,10 +20,24 @@ export default function EditUser() {
     const fetchData = async () => { 
       const response = await fetch("http://localhost:5001/api/user/"+userName);
       const newData = await response.json();  
+
       setUsername(newData['userName'])
-      console.log(username)
+      setBio(newData['bio'])
+      setPhoto(newData['photo'])
+
     };
     fetchData();
+
+      const fetchData2 = async () => { 
+      const response2 = await fetch("http://localhost:5001/api/user/bookmarks"+userName);
+      const newData2 = await response2.json();  
+     setPoster(newData2['Poster'])
+    };
+    fetchData2();
+
+
+
+
   },[username]);
 
   return (
@@ -34,22 +49,21 @@ export default function EditUser() {
             <MDBCard>
               <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
                 <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
-                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                  <MDBCardImage src={photo}
                     alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
-                  <MDBBtn outline color="dark" style={{height: '36px', overflow: 'visible'}}>
+                  <MDBBtn outline color="light" style={{height: '36px', overflow: 'visible'}}>
                     Edit profile
                   </MDBBtn>
                 </div>
                 <div className="ms-3" style={{ marginTop: '130px' }}>
                   <MDBTypography tag="h5">{username}</MDBTypography>
-                  <MDBCardText>New York</MDBCardText>
                 </div>
               </div>
               <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
                 <div className="d-flex justify-content-end text-center py-1">
                   <div>
                     <MDBCardText className="mb-1 h5">253</MDBCardText>
-                    <MDBCardText className="small text-muted mb-0">Photos</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Number of bookmarks</MDBCardText>
                   </div>
                   <div className="px-3">
                     <MDBCardText className="mb-1 h5">1026</MDBCardText>
@@ -65,9 +79,7 @@ export default function EditUser() {
                 <div className="mb-5">
                   <p className="lead fw-normal mb-1">About</p>
                   <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
-                    <MDBCardText className="font-italic mb-1">Web Developer</MDBCardText>
-                    <MDBCardText className="font-italic mb-1">Lives in New York</MDBCardText>
-                    <MDBCardText className="font-italic mb-0">Photographer</MDBCardText>
+                    <MDBCardText className="font-italic mb-1">{bio}</MDBCardText>
                   </div>
                 </div>
                 <div className="d-flex justify-content-between align-items-center mb-4">
@@ -76,7 +88,7 @@ export default function EditUser() {
                 </div>
                 <MDBRow>
                   <MDBCol className="mb-2">
-                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
+                    <MDBCardImage src={poster}
                       alt="image 1" className="w-100 rounded-3" />
                   </MDBCol>
                   <MDBCol className="mb-2">
