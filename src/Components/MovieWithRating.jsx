0cb9f51287aecb63959fd15f44f024e55df26e09
mariabroemarstrand <mariabroemarstrand@gmail.com>
2022-12-12 13:58:46 +0100

    MovieWith + No Rating med korrekt link via router

--- src/Components/MovieWithRating.jsx
+++ src/Components/MovieWithRating.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
 
+const MovieWithRating = ({PopularMovie,handleClick, titleId}) =>{
 
-const MovieWithRating = ({PopularMovie,handleClick}) =>{
-  // let URLwithRightPORT= movie.url.replace (5001,3000);
-  console.log(PopularMovie);
+let movieUrl = "/movies/"+PopularMovie.titleId
+
 return (
-  
 <Card>
  <Card.Body>
-  {/* <Nav.Link href={URLwithRightPORT}>{movie.titleName}</Nav.Link> */}
+  <Nav.Link href={movieUrl}>{PopularMovie.titleName}</Nav.Link>
     Rating: {PopularMovie.rating}
     {PopularMovie.titleId}
   </Card.Body>
- </Card>
-
+ </Card>  
 )
 }
 export default MovieWithRating;
