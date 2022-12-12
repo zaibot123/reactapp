c9cf723860403cbbb5acf4d5b06d2c8bc1b4e229
Tobia <tobias@taekker.net>
2022-12-12 22:08:33 +0100

    "similar movies" virker men er hardcoded

--- src/Components/ListWithPoster.jsx
+++ src/Components/ListWithPoster.jsx
@@ -1,39 +1,43 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import useFetch from "./useFetch";
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import Spinner from 'react-bootstrap/Spinner';
 
 
-export default function ListWithPoster() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 mt-3">
-          <div className="card">
-            <div className="card-vertical">
-              <div className="img-square-wrapper">
-                <img
-                  className=""
-                  src="https://m.media-amazon.com/images/M/MV5BMTU0NTg0MDM2Nl5BMl5BanBnXkFtZTcwNzk5MDgzMQ@@._V1_SX300.jpg"
-                  alt="Card image cap"
-                />
-              </div>
-              <div className="card-body">
-                <h4 className="card-title">Card title</h4>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <p>
-                  Example from: https://codepen.io/SteveJRobertson/pen/POdvgz
-                </p>
-              </div>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+
+export default function ListOfPosters({listOfURL}){
+const {result,loading,error} = useFetch("http://localhost:5001/api/movies/tt5040224/similar");
+if (loading!=="false") {
+  return(
+   <Spinner style={{position: 'absolute', left:'50%', top: '50%',}}>
+       <span className="visually-hidden">Loading...</span>
+   </Spinner>
+   )
+  }
+else{
+return (
+  <ImageList sx={{ width: 500, height: 450 }}>
+  <ImageListItem key="Subheader" cols={2}>
+    <ListSubheader component="div">December</ListSubheader>
+  </ImageListItem>
+  {result.map((item) => (
+    <ImageListItem key={item.poster}>
+      <img
+        src={`${item.poster}?w=248&fit=crop&auto=format`}
+        srcSet={`${item.poster}?w=248&fit=crop&auto=format&dpr=2 2x`}
+        alt={item.titleName}
+        loading="lazy"
+      />
+      <ImageListItemBar
+        title={item.titleName}
+        subtitle={item.plot}
+      />
+    </ImageListItem>
+  ))}
+</ImageList>
+);
 }
-
+}
