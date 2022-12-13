352a03815ac77b87752450708d695ccf6a2a4d40
mariabroemarstrand <mariabroemarstrand@gmail.com>
2022-12-13 14:41:27 +0100

    .

--- src/Components/TitleList.jsx
+++ src/Components/TitleList.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Title from "./Title";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const TitleList =  ({listOfResults}) =>{
+
+    const first5Movies =  listOfResults.slice(0, 5);
+    const last5Movies = listOfResults.slice(5, 10);
 return (
  <>
 <div className="list-title">
     <h1>Movies</h1>
 </div>
- {listOfResults.map(x => <Title all={x} key={x.url}/>)}
+
+    <Row >
+    <Col >   {first5Movies.map(x => <Title all={x} key={x.url}/>)}</Col>
+    <Col>   {last5Movies.map(x => <Title all={x} key={x.url}/>)}</Col>
+    </Row>
+
+ {/* {listOfResults.map(x => <Title all={x} key={x.url}/>)}  */}
 </>
 )
 }
