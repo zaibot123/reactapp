771f408b5e80a8cebc6c2f1d5b7da9edd46ce34b
mariabroemarstrand <mariabroemarstrand@gmail.com>
2022-12-17 11:37:36 +0100

    .

--- src/Components/MovieWithRating.jsx
+++ src/Components/MovieWithRating.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
+import Route, { Navigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
 
 const MovieWithRating = ({PopularMovie}) =>{
 let movieUrl = "/movies/"+PopularMovie.titleId
-
+const navigate = useNavigate();
 return (
 <Card>
  <Card.Body>
-  <Nav.Link href={movieUrl}>{PopularMovie.titleName}</Nav.Link>
+  <Nav.Link onClick={()=>navigate(movieUrl)}>{PopularMovie.titleName}</Nav.Link>
     Rating: {PopularMovie.rating}
   </Card.Body>
  </Card>  
