0f26a4c3cd9da7f8c8cfdd1567d260098d43bbbb
mariabroemarstrand <mariabroemarstrand@gmail.com>
2022-12-18 16:02:38 +0100

    best match done

---
+++ src/Components/BestSearch.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState, useContext } from "react";
+import TitleList from "./TitleList";
+import UsernameContext from "./UsernameContext";
+import { useParams } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
+import Row from 'react-bootstrap/Row';
+import { Button } from "react-bootstrap";
+import Col from 'react-bootstrap/Col';
+
+
+
+
+function BestSearch() {
+  let { getUser, setAUser } = useContext(UsernameContext);
+  let [status, setStatus] = useState('Loading')
+  const { search } = useParams()
+  let [alt, setAlt] = useState(null);
+
+
+
+  const fetchPage = async () => {
+    let url = "http://localhost:5001/api/movies?searchtype=best&username=" + getUser + "&title=" + search;
+    setStatus("Loading");
+    console.log(url);
+    const response = await fetch(url)
+    const Data = await response.json();
+    setStatus("Done");
+    setAlt(Data);
+  }
+
+  useEffect(() => {
+    fetchPage();
+  }, []);
+
+  if (status === "Done") {
+
+   
+
+    return (
+      <>
+        <TitleList listOfResults={alt} />
+
+      </>
+
+    )
+
+  } else {
+    return (
+      <Spinner style={{ position: 'absolute', left: '50%', top: '50%', }}>
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    )
+  }
+}
+export default BestSearch;
--- src/Components/TitleList.jsx
+++ src/Components/TitleList.jsx
@@ -5,8 +5,14 @@ import Col from 'react-bootstrap/Col';
 
 const TitleList =  ({listOfResults}) =>{
 
+console.log(listOfResults)
+
     const first5Movies =  listOfResults.slice(0, 5);
     const last5Movies = listOfResults.slice(5, 10);
+
+ 
+
+
 return (
  <>
 <div className="list-title">
@@ -14,8 +20,8 @@ return (
 </div>
 
     <Row >
-    <Col >   {first5Movies.map(x => <Title all={x} key={x.url}/>)}</Col>
-    <Col>   {last5Movies.map(x => <Title all={x} key={x.url}/>)}</Col>
+    <Col >   {first5Movies.map(x => <Title all={x} key={x.titleId}/>)}</Col>
+    <Col>   {last5Movies.map(x => <Title all={x} key={x.titleId}/>)}</Col>
     </Row>
 
 </>
