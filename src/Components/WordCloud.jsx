a8013f4a640c2de34354825427e4ca54241fb305
mariabroemarstrand <mariabroemarstrand@gmail.com>
2022-12-18 11:03:58 +0100

    wordcloud

---
+++ src/Components/WordCloud.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import ReactWordcloud from 'react-wordcloud';
+ 
+ 
+export default function Wordcloud({DataUrl}) {
+    let[data, setData]= useState(null);
+
+
+    useEffect(() => {
+          
+          const fetchData = async () => 
+          {
+          const response = await fetch(DataUrl);
+          const Data = await response.json();
+          setData(Data)
+          };
+
+          fetchData();
+      },[]);
+
+  return <ReactWordcloud words={words} />
+}
