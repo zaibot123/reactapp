53962b3e9f7ef58931e191efe4ba88e49b509569
mariabroemarstrand <mariabroemarstrand@gmail.com>
2022-12-19 12:28:31 +0100

    Word cloud / uden hard-code

--- src/Components/WordCloud.jsx
+++ src/Components/WordCloud.jsx
@@ -1,32 +1,10 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import ReactWordcloud from 'react-wordcloud';
-
-
-
-
-
-//   const MyComponent = () =>
-//     <div> 
-//       ...
-//       <ReactWordcloud words={words} />
-//       ...
-//    </div>;
-
-//   export default MyComponent;
- 
  
 export default function Wordcloud({DataUrl}) {
     let[data, setData]= useState(null);
-    const words = [
-        { text: 'told',    value: 64, },
-        { text: 'mistake', value: 11, },
-        { text: 'thought', value: 16, },
-        { text: 'bad',     value: 17, }
-      ];
     
-
-
     useEffect(() => {
           
           const fetchData = async () => 
